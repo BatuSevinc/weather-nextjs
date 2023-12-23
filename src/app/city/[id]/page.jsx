91cddc89b2fb7f608f0f2d6c19357f6e3fbb85ba
BatuSevinc <batusvnc1@gmail.com>
2023-12-23 13:20:11 +0300

    Changed background images

--- src/app/city/[id]/page.jsx
+++ src/app/city/[id]/page.jsx
@@ -1,44 +1,42 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import { useCity } from '../../context';
-import axios from 'axios';
-import {Info, Navbar,
-  Cloud,
-  Normal,
+"use client";
+import React, { useEffect, useState } from "react";
+import { useCity } from "../../context";
+import axios from "axios";
+import {
+  Info,
+  Navbar,
   Lightning,
-  Mist,
-  Rain,
-  Snow,
-  Sun,
-  Windy,
-  CitySkeleton,
   filterDailyWeather,
-  Cards
-} from '../../helpers'
-import Image from 'next/image';
-
+  Cards,
+} from "../../helpers";
+import Image from "next/image";
 
 const Page = () => {
   const { selectedCity } = useCity();
-  const [datas,setDatas] = useState(null)
-  const [bgImage,setBgImage] = useState(null)
+  const [datas, setDatas] = useState(null);
+  const [bgImage, setBgImage] = useState(Lightning);
   const [dailyForecast, setDailyForecast] = useState(null);
   const [allForecastsAt12Hour, setAllForecastsAt12Hour] = useState([]);
-
+  const [loading,setLoading] = useState(false)
   useEffect(() => {
+    setLoading(true)
     if (selectedCity) {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity.name || selectedCity}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${
+        selectedCity.name || selectedCity
+      }&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
       axios.get(url).then((response) => {
         setDatas(response.data);
       });
-  
-      const weeklyForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${selectedCity.name || selectedCity}&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
+
+      const weeklyForecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${
+        selectedCity.name || selectedCity
+      }&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}`;
       axios.get(weeklyForecastUrl).then((response) => {
         setDailyForecast(response.data);
       });
+      setLoading(false)
     }
   }, [selectedCity]);
-  
 
   useEffect(() => {
     if (dailyForecast && dailyForecast.list) {
@@ -47,10 +45,10 @@ const Page = () => {
 
       const updatedAllForecastsAt12Hour = [];
 
-      Object.keys(daysForecasts).forEach(day => {
+      Object.keys(daysForecasts).forEach((day) => {
         const forecastsForDay = daysForecasts[day];
 
-        const forecastsAt12 = forecastsForDay.filter(forecast => {
+        const forecastsAt12 = forecastsForDay.filter((forecast) => {
           const date = new Date(forecast.dt_txt);
           return date.getHours() === 12;
         });
@@ -61,59 +59,21 @@ const Page = () => {
       setAllForecastsAt12Hour(updatedAllForecastsAt12Hour);
     }
   }, [dailyForecast]);
-  
-  useEffect(() => {
-    if(datas && datas.weather){
-    const determineBgImage = async () => {
-      if (datas.weather[0].main === "Clouds") {
-        setBgImage(Cloud);
-      } else if (datas.weather[0].main === "Rain") {
-        setBgImage(Rain);
-      } else if (
-        datas.weather[0].main === "Mist" ||
-        datas.weather[0].main === "Fog" ||
-        datas.weather[0].main === "Smoke" ||
-        datas.weather[0].main === "Haze"
-      ) {
-        setBgImage(Mist);
-      } else if (datas.weather[0].main === "Snow") {
-        setBgImage(Snow);
-      } else if (datas.weather[0].main === "Windy") {
-        setBgImage(Windy);
-      } else if (
-        datas.weather[0].main === "Squall" ||
-        datas.weather[0].main === "Thunderstorm"
-      ) {
-        setBgImage(Lightning);
-      }
-      else{
-        setBgImage(Normal);
-      }
-    }
-    determineBgImage()
-    }
-  }, [datas]);
-
-  if (!bgImage) {
-    return <div className='absolute h-screen bg-black text-white w-screen top-0 bottom-0 left-0 right-0 flex justify-center items-center'>
-      <CitySkeleton/>
-    </div>
-  }
 
   return (
     <div>
-       <Image
+      <Image
         src={bgImage}
         quality={100}
-        alt='bg-image'
+        alt="bg-image"
         className="-z-10 object-cover"
         fill
         loading="eager"
         priority={true}
       />
-      <Navbar weather={datas} selectedCity={selectedCity}/>
-      <Info weather={datas} selectedCity={selectedCity}/>
-      <Cards allForecastsAt12Hour={allForecastsAt12Hour}/>
+      <Navbar weather={datas} selectedCity={selectedCity} />
+      <Info weather={datas} selectedCity={selectedCity} />
+      <Cards allForecastsAt12Hour={allForecastsAt12Hour} />
     </div>
   );
 };
