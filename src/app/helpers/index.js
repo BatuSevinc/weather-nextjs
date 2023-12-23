ca109a1acfed0623ad55b8aa6b5dab5bbdec63be
BatuSevinc <batusvnc1@gmail.com>
2023-12-23 13:19:13 +0300

    Updated import

--- src/app/helpers/index.js
+++ src/app/helpers/index.js
@@ -8,33 +8,18 @@ import getSunRise from './getSunSetSunRise'
 import getSunSet from './getSunSetSunRise'
 import filterDailyWeather from './dailyWeather'
 import { IoArrowBack } from "react-icons/io5"
-import Cloud from '../../../public/cloud.jpg'
-import Normal from '../../../public/normal.jpg'
 import Lightning from '../../../public/lightning.jpg'
-import Mist from '../../../public/mist.jpg'
-import Rain from '../../../public/rain.jpg'
-import Snow from '../../../public/snow.jpg'
-import Sun from '../../../public/sun.jpg'
-import Windy from '../../../public/windy.jpg'
-import Spinners from '../../../public/spinner.gif'
-
+import CloudIcons from '../../../public/cloud-icons.png'
 export {
   Datas,
   Navbar,
   Info,
   CitySkeleton,
   Cards,
-  Cloud,
-  Normal,
   Lightning,
-  Mist,
-  Rain,
-  Snow,
-  Sun,
-  Windy,
-  Spinners,
   getSunRise,
   getSunSet,
   filterDailyWeather,
   IoArrowBack,
+  CloudIcons
 }
